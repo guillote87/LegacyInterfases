f5d0edffe85c3d64bca526dbd2b59b401e19377f
Guillermo Quattrocchi <gq4dev@gmail.com>
2023-07-12 16:09:01 +0000

    Filtros Errores

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -19,17 +19,31 @@ sap.ui.define([
                 this.getView().setModel(oModelErrors, "Errors");
             },
             pressTile: function (event) {
-
                 var sPath = event.getSource().getBindingContext().getPath()
                 var oModel = this.getView().getModel()
-
+                var oModelErrors = this.getView().getModel("Errors")
                 var oContext = oModel.getProperty(sPath)
-                var oFilter = new Filter("Interfase", FilterOperator.EQ , oContext.id)
-                console.log(oFilter)
+
+                const info = oModelErrors.getData()
+
+                var FilterData = new JSONModel(info.filter(element => element.Interfase == oContext.id))
+
+                this.getView().setModel(FilterData, "FilteredErrors")
+
 
                 var table = this.byId("interfaseTable")
-                table.getBinding("items").filter(oFilter)
+
+
+                // Cambiar a visible los elementos
                 table.setProperty('visible').true
+                this.byId("search").setProperty('visible').true
+            },
+            searchFilter: function () {
+                var filter = this.byId("searchFilter").getValue()
+
+                var oFilter = new Filter("Usuario", FilterOperator.Contains, filter)
+                var table = this.byId("interfaseTable")
+                table.getBinding("items").filter(oFilter)
             }
         });
     });
