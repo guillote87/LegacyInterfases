522a1a08554aa037966f905a858e8973719aa9d0
Guillermo Quattrocchi <gq4dev@gmail.com>
2023-07-19 16:03:40 +0000

    Con la data

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -11,45 +11,81 @@ sap.ui.define([
     function (Controller, MessageToast, JSONModel, Filter, FilterOperator) {
         "use strict";
 
-        return Controller.extend("legacy.controller.MainView", {
+        return Controller.extend("project1.controller.MainView", {
             onInit: function () {
+
                 var oModel = new JSONModel("../model/tileError.json");
                 this.getView().setModel(oModel);
-                var oModelErrors = new JSONModel("../model/interfaseError.json");
-                this.getView().setModel(oModelErrors, "Errors");
+                this.byId('interfasesFull').setBusy(true)
+                var interModel = this.getOwnerComponent().getModel('interfasesModel')
+
+                interModel.setHeaders({
+                    "sap-client": "300"
+                })
+
+                interModel.read("/LogsSet", {
+                    filters: [
+                        new sap.ui.model.Filter("Interface", FilterOperator.EQ, '06')
+                    ],
+                    success: function (data) {
+                        console.log("DATOS", data.results)
+                        this.getView().setModel(new JSONModel(data.results), "interfases")
+                        this.byId('interfasesFull').setBusy(false)
+
+                    }.bind(this),
+                    error: function (e) {
+                        //
+                    }
+                });
             },
             pressTile: function (event) {
-                var sPath = event.getSource().getBindingContext().getPath()
-                var oModel = this.getView().getModel()
-                var oModelErrors = this.getView().getModel("Errors")
-                var oContext = oModel.getProperty(sPath)
-
-                const info = oModelErrors.getData()
-
-                var FilterData = new JSONModel(info.filter(element => element.Interfase == oContext.id))
-
-                this.getView().setModel(FilterData, "FilteredErrors")
-
-
                 var table = this.byId("interfaseTable")
+          
+                table.setBusy(true)
+
+                var sPath = event.getSource().getBindingContext().sPath
+                var oModel = this.getView().getModel()
+                var oContext = oModel.getProperty(sPath);
+                console.log(oContext)
+
+                var filterInterface = this.getOwnerComponent().getModel('interfasesModel')
+
+                filterInterface.setHeaders({
+                    "sap-client": "300"
+                })
+
+                filterInterface.read("/LogsSet", {
+                    filters: [
+                        new sap.ui.model.Filter("Interface", FilterOperator.EQ, oContext.id),
+                    ],
+                    success: function (data) {
+                        this.getView().setModel(new JSONModel(data.results), "FilteredErrors")
+                        table.setBusy(false)
+                    }.bind(this),
+                    error: function (e) {
+                        //
+                    }
+                })
+
 
 
                 // Cambiar a visible los elementos
-                table.setProperty('visible').true
-                this.byId("search").setProperty('visible').true
+                table.setVisible(true)
+                this.byId("search").setVisible(true)
+                this.byId("interfasesFull").setVisible(false)
             },
+
+
             searchFilter: function () {
+                this.byId("interfaseTable").setBusy(true)
 
                 var aFilters = [];
                 var filter = this.byId("searchFilter").getValue()
 
-                var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "dd.MM.YYYY" });
+                var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "YYYYMMdd" });
                 var fromDate = dateFormat.format(this.getView().byId("fecha").getDateValue());
                 var toDate = dateFormat.format(this.getView().byId("fecha").getSecondDateValue());
 
-
-
-                console.log(fromDate, toDate)
                 if (filter) {
                     aFilters.push(new Filter("OrgVentas", FilterOperator.Contains, filter))
                 }
@@ -59,10 +95,8 @@ sap.ui.define([
                 }
 
                 this.byId("interfaseTable").getBinding("items").filter(aFilters);
-               // this.getView().byId("fecha").setValue(null);
+                this.byId("interfaseTable").setBusy(false)
 
-                //  var table = this.byId("interfaseTable")
-                //  table.getBinding("items").filter(oFilter)
             },
 
 
